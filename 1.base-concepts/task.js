1c7ad2dcc9d57f3f73af94ff9b2e826015281646
Олег Жебокрицкий <oleg149@yandex.ru>
2023-04-09 11:34:45 +0300

    Первое задание полностью готово

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,10 +1,38 @@
 "use strict"
+
 function solveEquation(a, b, c) {
-  let arr = [];
-  
-  return arr;
+	let arr = [];
+	let discr = (b ** 2) - (4 * a * c);
+
+	if (discr === 0) {
+		let sqrt = -b / (2 * a);
+		arr.push(sqrt);
+		console.log(arr);
+	} else if (discr > 0) {
+		let sqrt1 = (-b + Math.sqrt(discr)) / (2 * a);
+		let sqrt2 = (-b - Math.sqrt(discr)) / (2 * a);
+		arr.push(sqrt1, sqrt2);
+		console.log(arr);
+	} else if (discr < 0) {
+		return [];
+	}
+	return arr;
+
 }
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  
+
+	function isNumber(percent, contribution, amount, countMonths) {
+		if (typeof percent, contribution, amount, countMonths != "string") return false;
+		return !isNaN(arguments) && !isNaN(parseFloat(arguments))
+	}
+
+	let transformPercent = percent / 100;
+	let bodyCredit = amount - contribution;
+	let monthPercent = transformPercent / 12;
+	let everymonthPay = bodyCredit * (monthPercent + (monthPercent / (((1 + monthPercent) ** countMonths) - 1)));
+	let totalPay = everymonthPay * countMonths;
+	return +totalPay.toFixed(2);
 }
+
+console.log(calculateTotalMortgage(10, 0, 50000, 12));
